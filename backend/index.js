c9c6e1b7f301a176b1c4c9271ee535b0d78c10ba
Adam Gonzales <adamgonzales1@gmail.com>
2023-10-11 15:38:21 -0600

    feature: add get route for all campgrounds

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,29 @@ import { PORT } from './config.js'
 const app = express()
 import 'dotenv/config'
 import mongoose from 'mongoose'
+import { Campground } from './models/campground.js'
 
 app.get('/', (req, res) => {
   res.send('YelpCamp Version 3')
 })
+
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find()
+  console.log(campgrounds)
+  return res.status(200).json({
+    count: campgrounds.length,
+    data: campgrounds,
+  })
+})
+
+// const newCampground = new Campground({
+//   name: 'Desert Wonderland',
+//   price: 100,
+//   description: 'Hope you like scorpions',
+//   location: 'El Paso, TX',
+// })
+
+// newCampground.save()
 
 mongoose
   .connect(process.env.mongoDBURL)
