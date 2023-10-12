a6c47ca74f1e298e91a8746c72a428087e4b12bd
Adam Gonzales <adamgonzales1@gmail.com>
2023-10-12 10:58:59 -0600

    feature: add put and delete route

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,15 @@ import 'dotenv/config'
 import mongoose from 'mongoose'
 import { Campground } from './models/campground.js'
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.send('YelpCamp Version 3')
 })
 
 app.get('/campgrounds', async (req, res) => {
-  const campgrounds = await Campground.find()
+  const campgrounds = await Campground.find({})
   console.log(campgrounds)
   return res.status(200).json({
     count: campgrounds.length,
@@ -18,8 +21,8 @@ app.get('/campgrounds', async (req, res) => {
   })
 })
 
-app.get('/campgrounds/:id/new', async (req, res) => {
-  const campground = await Campground.findById(req.params.id)
+app.get('/campgrounds/:id/edit', (req, res) => {
+  // edit campground form
 })
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -30,7 +33,11 @@ app.get('/campgrounds/:id', async (req, res) => {
   })
 })
 
-app.post('/campgrounds/new', async (req, res) => {
+app.get('/campgrounds/new', async (req, res) => {
+  // new campground form
+})
+
+app.post('/campgrounds', async (req, res) => {
   try {
     if (
       !req.body.name ||
@@ -49,6 +56,44 @@ app.post('/campgrounds/new', async (req, res) => {
   } catch (error) {
     console.log(error)
     res.status(500).send({ message: error.message })
+  }
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+  try {
+    if (
+      !req.body.name ||
+      !req.body.price ||
+      !req.body.description ||
+      !req.body.location
+    ) {
+      return res.status(400).send({
+        message:
+          'Please send all required fields: name, price, description, location',
+      })
+    }
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, req.body)
+    if (!campground) {
+      return res.status(404).json({ message: 'Campground not found' })
+    }
+    return res.status(200).send({ message: 'Campground updated successfully' })
+  } catch (error) {
+    return res.status(500).send({ message: error.message })
+  }
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  try {
+    const campground = await Campground.findByIdAndDelete(req.params.id)
+    if (!campground) {
+      return res.status(404).json({ message: 'Campground not found' })
+    }
+    return res
+      .status(200)
+      .send({ message: `Campground ${campground.name} deleted successfully` })
+  } catch (error) {
+    return res.status(500).send({ message: error.message })
   }
 })
 
